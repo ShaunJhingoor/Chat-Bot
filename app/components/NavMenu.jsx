9415fdc0e975f4c3eb6be533ad9fef42884ebf6a
Shaun Jhingoor <Jhingoor1945@gmail.com>
2024-08-09 17:24:12 -0400

    styling

--- app/components/NavMenu.jsx
+++ app/components/NavMenu.jsx
@@ -2,23 +2,36 @@
 import { signOut, useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
 import { redirect } from "next/navigation";
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./NavMenu.css"
 
 function AuthButton() {
   const { data: session } = useSession();
 
   if (session) {
     return (
-      <>
-        {session?.user?.name} <br />
-        <button
+      <div
+      style={{
+      width: '100vw',
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'nowrap',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      color: 'Black', 
+    }}>
+        {session?.user?.name}
+        <FontAwesomeIcon
+        icon={faSignOutAlt}
           onClick={() => {
             signOut();
             redirect("/");
           }}
+          className="sign-out-icon" 
         >
-          Sign Out
-        </button>
-      </>
+        </FontAwesomeIcon>
+      </div>
     );
   }
   if (!session) {
@@ -26,10 +39,22 @@ function AuthButton() {
   }
   console.log(session);
 }
+
 export default function NavMenu() {
   const pathname = usePathname();
+  
   return (
-    <div style={{ "width": "90vw",fontWeight: "bold"}}>
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '1rem',
+        color: 'white', 
+      }}
+    >
       <AuthButton />
     </div>
   );
